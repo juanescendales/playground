98625b94f2895498884146e91cd078e8d4093c5b
Juan Cendales <juanescendales@gmail.com>
2025-06-09 14:28:14 -0500

    feat: o(n2) solution with better complexity in time and memory

--- programming-challenges/leetcode/5-longest-palindromic-substring/solution.go
+++ programming-challenges/leetcode/5-longest-palindromic-substring/solution.go
@@ -9,6 +9,39 @@ func main() {
 }
 
 func longestPalindrome(s string) string {
+	var oddPalindrome, evenPalindrome string
+	maxPalindrome, size := s[:1], len(s)
+	for k := range size - 1 {
+		oddPalindrome = expandPalindrome(s, k, k)
+		if len(oddPalindrome) > len(maxPalindrome) {
+			maxPalindrome = oddPalindrome
+		}
+		evenPalindrome = expandPalindrome(s, k, k+1)
+		if len(evenPalindrome) > len(maxPalindrome) {
+			maxPalindrome = evenPalindrome
+		}
+	}
+	return maxPalindrome
+}
+
+func expandPalindrome(s string, c1, c2 int) string {
+	l, r := c1, c2
+	size := len(s)
+	palindrome, palindromeLen := "", 0
+
+	for (l >= 0 && r < size) && (s[l] == s[r]) {
+		newLength := r - l + 1
+		if newLength > palindromeLen {
+			palindrome = s[l : r+1]
+			palindromeLen = newLength
+		}
+		l--
+		r++
+	}
+	return palindrome
+}
+
+func LongestPalindromeO3Solution(s string) string {
 	var j, size int
 	letters := []rune(s)
 	size = len(letters)
